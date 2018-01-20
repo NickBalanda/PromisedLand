ef2b1104bcafad405782fffbdc3d55299d641b7c
NickBalanda <nicholasmbalanda@gmail.com>
2018-01-20 12:13:02 -0200

    NavMesh
    Navmesh tutorial

---
+++ OpenSea/Assets/Scripts/PlayerClickToMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerClickToMove : MonoBehaviour {
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0)) {
+            if(Physics.Raycast(ray, out hit, 100)) {
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
