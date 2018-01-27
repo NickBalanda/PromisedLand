26d698f7f9908ce6048be1c3c62ec7c069020006
NickBalanda <nicholasmbalanda@gmail.com>
2018-01-27 00:55:37 -0200

    Editor scripting
    Examples of editor scripting

---
+++ OpenSea/Assets/HelperEditorScripts/DeleteAllPlayerPrefs.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DeleteAllPlayerPrefs : MonoBehaviour {
+
+    [MenuItem("Utils/Delete All PlayerPrefs")]
+    static public void DeletePlayerPrefs() {
+        PlayerPrefs.DeleteAll();
+    }
+}
---
+++ OpenSea/Assets/HelperEditorScripts/Editor/SpawnObjectEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SpawnObject))]
+public class SpawnObjectEditor : Editor {
+
+    public override void OnInspectorGUI() {
+
+        EditorGUILayout.HelpBox("Use this component to spawn an object at defined position", MessageType.Info);
+
+        DrawDefaultInspector();
+
+        SpawnObject myScript = (SpawnObject)target;
+
+        EditorGUILayout.Space();
+
+        if(GUILayout.Button("Spawn Object")) {
+            myScript.BuildObject();
+        }
+    }
+}
