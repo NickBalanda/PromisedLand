323a940256a9910608f94a42adc921c52fc637e9
Nicholas Balanda <nicholasmbalanda@gmail.com>
2018-03-23 22:58:26 -0300

    Fungus
    Created a switch for use on puzzles
    
    And fungus for tutorial and dialog

---
+++ OpenSea/Assets/Scripts/ButtonSwitch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonSwitch : Interactable {
+
+    //materials to give feedback on button state
+    public Material onMaterial;
+    public Material offMaterial;
+
+    //is the button on?
+    public bool isOn;
+
+    //can you turn the button on and off, or press it once?
+    public bool toggleButton = true;
+
+    [Space(10)]
+    [SerializeField] 
+    public UnityEvent onButtonPressed = new UnityEvent(); 
+
+    [SerializeField] 
+    public UnityEvent offButtonPressed = new UnityEvent();
+
+    private MeshRenderer mr;
+
+    void Start () {
+        mr = GetComponent<MeshRenderer>();
+        if (isOn) {
+            mr.material = onMaterial;
+        } else {
+            mr.material = offMaterial;
+        }
+	}
+
+    public override void Interact() {
+        base.Interact();
+        isOn = !isOn;
+
+        if (isOn) {
+            mr.material = onMaterial;
+            onButtonPressed.Invoke();
+        } else {
+            mr.material = offMaterial;
+            offButtonPressed.Invoke();
+        }
+
+        if (!toggleButton) {
+            enabled = false;
+        }
+    }
+}
---
+++ OpenSea/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour {
+	
+	[Tooltip("Function called when trigger enter")]
+	[SerializeField] // Serialize to show field in the editor
+	public UnityEvent onTriggerEnter = new UnityEvent(); // our Unity Event
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+            onTriggerEnter.Invoke();
+            Destroy(GetComponent<TriggerEvent>());
+        }
+	}
+
+}
